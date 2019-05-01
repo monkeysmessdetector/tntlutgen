0ffb737e9dc49a95ebd65e729048c9446038f378
Earthcomputer <burtonjae@hotmail.co.uk>
2019-05-01 14:07:12 +0100

    Add the addend

--- src/SimConstantFinder.java
+++ src/SimConstantFinder.java
@@ -3,17 +3,20 @@ public class SimConstantFinder {
     public static void main(String[] args) {
         final long mask = (1L << 48) - 1;
         final long base = (0x5deece66dL * 0x5deece66dL) & mask;
-        long constant = base;
+        final long baseAddend = (0x5deece66dL * 0xbL + 0xbL) & mask;
+        long multiplier = base;
+        long addend = baseAddend;
         int power = 1;
         int minBitCount = 49;
 
         while (true) {
-            int bitCount = Long.bitCount(constant);
+            int bitCount = Long.bitCount(multiplier);
             if (bitCount < minBitCount) {
                 minBitCount = bitCount;
-                System.out.println(power + " (" + (power / 4) + " items): " + Long.toBinaryString(constant));
+                System.out.println(power + " (" + (power / 4) + " items): " + Long.toBinaryString(multiplier) + " plus " + Long.toHexString(addend));
             }
-            constant = (constant * base) & mask;
+            multiplier = (multiplier * base) & mask;
+            addend = (base * addend + baseAddend) & mask;
             power++;
         }
     }
